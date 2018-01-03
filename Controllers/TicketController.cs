6fc39c4256a4f979a4ec9bb941d8a17503ec3119
Guest User <Guest@epicodus-107.local>
2018-01-02 16:05:06 -0800

    initial files

---
+++ Controllers/TicketController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvc.Controllers
+{
+    public class TicketController : Controller
+    {
+        [HttpGet("/tix")]
+        public IActionResult Index()
+
+        {
+            var s = new Seat() { Location = "Orchestra", Price = 300 };
+
+
+            return View(s);
+        }
+
+	    public string Index2()
+        {
+            return "Hello from Tickets!";
+         }
+
+    }
+}
---
+++ Models/Seat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace mvc.Models
+{
+    public class Seat
+    {
+        public string Location
+        {
+            get;
+            set;
+        }
+
+        public double Price
+        {
+            get;
+            set;
+        }
+    }
+}
